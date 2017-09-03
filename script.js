94a7e085e77f9617d1086853ac917ce8f0bf1c71
burry <burry439@gmail.com>
2017-09-03 14:46:30 +0300

    made beer app more module

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-(function(){
+function BeerReviewApp(){
+
 var beers = []
 var postBeer = $(".post-beer")
 var sort = $("#sort")
 var isA = false;
+
 
 function addBeer(name, category, rate) {
     var beer = {
@@ -14,19 +16,7 @@ function addBeer(name, category, rate) {
     beers.push(beer)
 }
 
-postBeer.click(function () {
-    var beerN = $("#name").val()
-    var beerT = $("#cat").val()
-    var rate = $("#rate").val()
-    if(beerN == "" || (beerT == "") || rate == "Rate"){
-        alert("Pleas fill out")
-    } else {
-    addBeer(beerN, beerT, rate)
-    updateBeers()
-    $("#name").val("") 
-    $("#cat").val("")
-    }
-})
+
 function updateBeers() {
     $(".beers-list").empty();
     for (i = 0; i < beers.length; i++) {
@@ -35,24 +25,58 @@ function updateBeers() {
 $(".edit").click(edit)
 }
 
-sort.click(function () {
-    isA = !isA;
-    beers.sort(function compare(a, b) {
-
-        if (a.rate < b.rate) {
-            return isA ? -1 : 1
-        } else if (a.rate > b.rate) {
-            return isA ? 1 : -1;
-        } else {
-            return 0;
-        }
-    })
-    updateBeers()
-})
 
 function edit(){
 var edit = $(".change").val()
 var list_new = $(".beers-list").html();
 $(".beers-list").empty().append('<li>'+ '<button type="button" class="edit">Edit</button>' +  '<input class="change">' + "Name: " + edit + " Category: , stars: " + '</input>' + " </li>"); 
 }
-})();
+   
+function sortBeer() {
+    
+        
+        isA = !isA;
+        beers.sort(function compare(a, b) {
+    
+            if (a.rate < b.rate) {
+                return isA ? -1 : 1
+            } else if (a.rate > b.rate) {
+                return isA ? 1 : -1;
+            } else {
+                return 0;
+            }
+        })
+        updateBeers()
+    }
+
+  return{
+    addBeer : addBeer,
+    updateBeers : updateBeers,
+    edit : edit,
+    sortBeer : sortBeer
+  }
+  
+
+}
+//click handelers
+
+var app = BeerReviewApp();
+
+
+$('#sort').click( app.sortBeer)
+
+
+
+   $(".post-beer").click(function () {
+    var beerN = $("#name").val()
+    var beerT = $("#cat").val()
+    var rate = $("#rate").val()
+    if(beerN == "" || (beerT == "") || rate == "Rate"){
+        alert("Pleas fill out")
+    } else {
+    app.addBeer(beerN, beerT, rate)
+    app.updateBeers()
+    $("#name").val("") 
+    $("#cat").val("")
+    }
+})
